4127ee7e52a9974122c81dfe039588880fd00206
Tao Zhang <forrestzhang1982@gmail.com>
2015-10-13 13:01:13 -0500

    remove code

--- startup.sh
+++ startup.sh
@@ -5,7 +5,7 @@ echo $BCBIO_UID $BCBIO_HOME $BCBIO_USER
 if ! id -u $BCBIO_USER > /dev/null 2>&1; then
   echo "use local user: " $BCBIO_USER
   adduser -u  $BCBIO_UID $BCBIO_USER
-if
+fi
 
 if [ -e $BCBIO_GENOMES ];
   then
@@ -23,28 +23,3 @@ for arg in "$*"
 do
   	exec  $*
 done
-
-# if [-e /mnt/transfer]
-#     then
-#         echo "/mnt/transfer exists."
-#         chown -R $BCBIO_UID:$BCBIO_GID /mnt/transfer
-# else
-#     mkdir -p /mnt/transfer
-#     chown -R $BCBIO_UID:$BCBIO_GID /mnt/transfer
-# fi
-#
-#
-# if [-e /mnt/biodata]
-#   then
-#     echo "/mnt/biodata exists."
-#     chown -R $BCBIO_UID:$BCBIO_GID /mnt/biodata
-#   else
-#     mkdir -p /mnt/biodata
-#     mv /usr/local/share/bcbio-nextgen/galaxy/bcbio_system.yaml /usr/local/share/bcbio-nextgen/config && \
-#     rmdir /usr/local/share/bcbio-nextgen/galaxy && \
-#     ln -s /mnt/biodata/galaxy /usr/local/share/bcbio-nextgen/galaxy && \
-#     ln -s /mnt/biodata/gemini_data /usr/local/share/bcbio-nextgen/gemini_data && \
-#     ln -s /mnt/biodata/genomes /usr/local/share/bcbio-nextgen/genomes && \
-#     ln -s /mnt/biodata/liftOver /usr/local/share/bcbio-nextgen/liftOver && \
-#     chown -R $BCBIO_UID:$BCBIO_GID /mnt/biodata
-# fi
